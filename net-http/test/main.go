534ef58cb3c1aa7d930c98b03e671876a74ffdf0
Mysuy <15075264+mysuy@user.noreply.gitee.com>
2026-06-02 18:28:09 +0800

    init

---
+++ net-http/test/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type statusResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// 重写 WriteHeader 方法，记录状态码
+func (rw *statusResponseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+// 请求日志中间件（Middleware Pattern）
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// 初始化默认状态码 200
+		wrappedWriter
+	})
+}
+
+func main() {
+}
